a85bd58aa898323580367f811a393ae9fb6ddafc
Shaurya Parmar <shaurya1726.becse24@chitkara.edu.in>
2025-01-28 19:28:30 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main(){
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
         printf("Vowel");
     }
-    else if(c>='a'||c<='z'||c>='A'||c>='Z'){
+    else if(c>='a'&&c<='z'||c>='A'&&c>='Z'){
         printf("Consonant");
     }
     else if(c>='0'&&c<='9'){
