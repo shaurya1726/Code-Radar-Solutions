10df2db9c25c539e1f1fbe9d47f5911053b076a2
Shaurya Parmar <shaurya1726.becse24@chitkara.edu.in>
2025-01-31 18:11:49 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch='A'){
+        printf("Excellent");
+    }
+    else if(ch='B'){
+        printf("Good");
+    }
+    else if(ch='C'){
+        printf("Average");
+    }
+    else if(ch='D'){
+        printf("Below Average");
+    }
+    else{
+        printf("Fail");
+    }
+    return 0;
+}
