5db6480975b418d09c7ccb1807361aa97604f50c
Shaurya Parmar <shaurya1726.becse24@chitkara.edu.in>
2025-02-01 15:24:44 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,21 +4,7 @@ int main(){
     char oper;
     scanf("%d%d%c",&x,&y,&oper);
     scanf("%*c",&oper);
-    switch(oper) {
-        case '+':
-        printf("%d",x+y);
-        break;
-        case '-':
-        printf("%d",x-y);
-        break;
-        case '*':
-        printf("%d",x*y);
-        break;
-        case '/':
-        printf("%d",x/y);
-        break;
-        default:
-        printf("error");
+    
     }
     return 0;
 }
